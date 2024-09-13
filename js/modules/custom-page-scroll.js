1228cef886e7059cce43ff972bb39b8bfbaae29a
MuraliJbk <murali@jbkinfotech.com>
2024-09-13 13:27:41 +0530

    srvc page 09

--- js/modules/custom-page-scroll.js
+++ js/modules/custom-page-scroll.js
@@ -3,7 +3,9 @@ const eleScroll = function (event) {
   event.preventDefault();
   let headerHeight = document.querySelector('header').clientHeight;
   const idName = event.target.getAttribute('href').substring(1);
+  console.log(idName);
   let targetDiv = document.querySelector(`#${idName}`);
+  if (!targetDiv) return;
   window.scroll({
     top: targetDiv.offsetTop - headerHeight,
     behavior: 'smooth',
@@ -13,13 +15,15 @@ window.onscroll = function () {
   const scroll = parseInt(this.scrollY);
 };
 
-let atags = document.querySelectorAll('a[href]');
-for (let atag of atags) {
+let atags = document.querySelectorAll('a[href]').forEach((atag) => {
   let href = atag.getAttribute('href');
-  if (href.includes('#')) {
+  let length = href.length > 1;
+  console.log(length);
+  let substring = href.substring(1);
+  if (href.includes(`#${substring}`) && length) {
     atag.addEventListener('click', function (e) {
       eleScroll(e);
     });
   }
-}
+});
 const pageScroll = function () {};
