952b7b98264635cc41f924c7eaa0b5821b4181d2
MuraliJbk <murali@jbkinfotech.com>
2024-09-13 15:27:42 +0530

    srvc page 09

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,59 @@
-const { config } = require("grunt");
-module.exports = function(grunt) {
+const { config } = require('grunt');
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     terser: {
       options: {
         compress: {
-          drop_console: true
+          drop_console: true,
         },
         mangle: true,
         output: {
-          comments: false
-        }
+          comments: false,
+        },
       },
       my_target: {
         files: {
-          'dist/core.bundle.js': ['dist/core.concat.js']
-        }
-      }
+          'dist/core.bundle.js': ['dist/core.concat.js'],
+        },
+      },
     },
 
     concat: {
       dist: {
         src: ['js/*.js', 'js/**/*.js'],
-        dest: 'dist/core.concat.js'
-      }
+        dest: 'dist/core.concat.js',
+      },
     },
 
     cssmin: {
       target: {
         files: {
-          'core.bundle.css': ['css/**/*.css', 'css/*.css']
-        }
-      }
+          'core.bundle.css': ['css/**/*.css', 'css/*.css'],
+        },
+      },
     },
 
     validation: {
       options: {
-        doctype: 'HTML5'
+        doctype: 'HTML5',
       },
       files: {
-        src: ['./*.html']
-      }
+        src: ['./*.html'],
+      },
     },
 
     watch: {
       stylesheets: {
         files: ['css/**/*.css', 'css/*.css'],
         tasks: ['cssmin'],
-        livereload: true
+        livereload: true,
       },
       scripts: {
         files: ['js/*.js', 'js/**/*.js'],
-        tasks: ['concat', 'terser']
-      }
-    }
+        tasks: ['concat', 'terser'],
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -63,5 +63,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-w3c-html-validation');
 
   // Default task(s).
-  grunt.registerTask('default', ['validation', 'concat', 'terser', 'cssmin', 'watch']);
+  grunt.registerTask('default', [
+    'validation',
+    'concat',
+    'terser',
+    'cssmin',
+    'watch',
+  ]);
 };
