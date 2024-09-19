ec447557665eec4eb0a1ebb64a7e830a37ad374c
MuraliJbk <murali@jbkinfotech.com>
2024-09-19 18:04:02 +0530

    test 17

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@
 import animation from './modules/animation';
 import stickyHeader from './modules/sticky';
 import scrollModule from './modules/scroll';
+import customSlick from './modules/custom-slick';
 
-animation.init();
-stickyHeader.init();
-scrollModule();
+document.addEventListener('DOMContentLoaded', function () {
+  animation.init();
+  stickyHeader.init();
+  scrollModule();
+  customSlick();
+});
