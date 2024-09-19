d911092b5b2d56d19826e64a8d21c70973fb8c52
unknown <ramlachu@jbkinfotech.com>
2024-09-19 20:29:35 +0530

    correction

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -1,23 +1,28 @@
-const customSlick = () => {
-  const $ele = document.querySelectorAll('.culture-main');
-  $ele.forEach(($culture) => {
-    console.log($culture, 'culture');
-    if (!$culture.classList.contains('slick-initialized')) {
-      $culture.slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow:
-          '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-        nextArrow:
-          '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-        dots: true,
-        speed: 1000,
-        infinite: false,
-        autoplay: false,
-        variableWidth: true,
-      });
-    }
-  });
-};
-export default customSlick;
+// const customSlick = () => {
+//   const $ele = document.querySelectorAll('.culture-main');
+//   $ele.forEach(($culture) => {
+//     console.log($culture, 'culture');
+//     const postAppend = $(".post-array-appends");
+//     if ($(window).width() <= 767) {
+//     if (!$culture.classList.contains('slick-initialized')) {
+//       $culture.slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         prevArrow:
+//           '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+//         nextArrow:
+//           '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+//         dots: false,
+//         speed: 1000,
+//         infinite: false,
+//         autoplay: false,
+//         variableWidth: true,
+//         appendArrows: postAppend,
+//         appendDots: postAppend,
+//       });
+//     }
+//     }
+//   });
+// };
+// export default customSlick;
