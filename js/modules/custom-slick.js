433e34cf671326efe6904dc08edc7194cac50ede
unknown <ramlachu@jbkinfotech.com>
2024-09-23 15:16:41 +0530

    correction

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -13,10 +13,36 @@ const customSlick = () => {
           slidesToScroll: 1,
           arrows: true,
           prevArrow:
-            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
           nextArrow:
-            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-          dots: false,
+            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
+          dots: true,  
+          speed: 1000,
+          infinite: false,
+          autoplay: false,
+          variableWidth: true,
+          appendArrows: postAppend,
+          appendDots: postAppend,
+        });
+      }
+    }
+  });
+
+  const $eles = $('.benefit-lists');
+  $eles.each(function () {
+    const $cultureSlider = $(this);
+    const postAppend = $(this).parent().children('.benefit-lists-appends');
+    if ($(window).width() <= 767) {
+      if (!$cultureSlider.hasClass('slick-initialized')) {
+        $cultureSlider.slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          prevArrow:
+            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
+          nextArrow:
+            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
+          dots: true,  
           speed: 1000,
           infinite: false,
           autoplay: false,
