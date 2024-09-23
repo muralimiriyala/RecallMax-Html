239225873cba1b014303afadaff56930b8300c6f
MuraliJbk <murali@jbkinfotech.com>
2024-09-23 11:06:50 +0530

    test

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -1,29 +1,48 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
 const customSlick = () => {
-  alert('test');
-  const $ele = document.querySelectorAll('.culture-main');
-  $ele.forEach(($culture) => {
-    console.log($culture, 'culture');
-    const postAppend = $('.post-array-appends');
-    if ($(window).width() <= 767) {
-      if (!$culture.classList.contains('slick-initialized')) {
-        $culture.slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: true,
-          prevArrow:
-            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-          nextArrow:
-            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-          dots: false,
-          speed: 1000,
-          infinite: false,
-          autoplay: false,
-          variableWidth: true,
-          appendArrows: postAppend,
-          appendDots: postAppend,
-        });
-      }
-    }
+  const postAppend = $('.post-array-appends');
+  // const $ele = document.querySelectorAll('.culture-main');
+  // $ele.forEach(($culture) => {
+  //   console.log($culture, 'culture slick123');
+  //   if ($(window).width() <= 767) {
+  //     if (!$culture.classList.contains('slick-initialized')) {
+  //       $culture.slick({
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         arrows: true,
+  //         prevArrow:
+  //           '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+  //         nextArrow:
+  //           '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+  //         dots: false,
+  //         speed: 1000,
+  //         infinite: false,
+  //         autoplay: false,
+  //         variableWidth: true,
+  //         appendArrows: postAppend,
+  //         appendDots: postAppend,
+  //       });
+  //     }
+  //   }
+  // });
+
+  $('.culture-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+      '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+    nextArrow:
+      '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
+    dots: false,
+    speed: 1000,
+    infinite: false,
+    autoplay: false,
+    variableWidth: true,
+    appendArrows: postAppend,
+    appendDots: postAppend,
   });
 };
 export default customSlick;
