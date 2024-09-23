9a7413efaea3bac520acc193a1cff78e32733384
unknown <ramlachu@jbkinfotech.com>
2024-09-23 15:35:25 +0530

    correction

--- js/modules/custom-slick.js
+++ js/modules/custom-slick.js
@@ -3,55 +3,6 @@ import 'slick-carousel';
 
 const customSlick = () => {
   const $ele = $('.culture-main');
-<<<<<<< HEAD
-  $ele.each(function () {
-    const $cultureSlider = $(this);
-    const postAppend = $(this).parent().children('.culture-main-appends');
-    if ($(window).width() <= 767) {
-      if (!$cultureSlider.hasClass('slick-initialized')) {
-        $cultureSlider.slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: true,
-          prevArrow:
-            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
-          nextArrow:
-            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
-          dots: true,  
-          speed: 1000,
-          infinite: false,
-          autoplay: false,
-          variableWidth: true,
-          appendArrows: postAppend,
-          appendDots: postAppend,
-        });
-      }
-    }
-  });
-
-  const $eles = $('.benefit-lists');
-  $eles.each(function () {
-    const $cultureSlider = $(this);
-    const postAppend = $(this).parent().children('.benefit-lists-appends');
-    if ($(window).width() <= 767) {
-      if (!$cultureSlider.hasClass('slick-initialized')) {
-        $cultureSlider.slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: true,
-          prevArrow:
-            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
-          nextArrow:
-            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
-          dots: true,  
-          speed: 1000,
-          infinite: false,
-          autoplay: false,
-          variableWidth: true,
-          appendArrows: postAppend,
-          appendDots: postAppend,
-        });
-=======
   function initializeSlider() {
     $ele.each(function () {
       const $cultureSlider = $(this);
@@ -63,10 +14,10 @@ const customSlick = () => {
             slidesToScroll: 1,
             arrows: true,
             prevArrow:
-              '<div class="slick-arrow aria-disabled="false" tabindex="0" role="button" slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            nextArrow:
-              '<div class="slick-arrow aria-disabled="false" tabindex="0" role="button" slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-light fa-sharp fa-arrow-right"></span></div>',
-            dots: false,
+            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
+          nextArrow:
+            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
+            dots: true,
             speed: 1000,
             infinite: false,
             autoplay: false,
@@ -75,7 +26,6 @@ const customSlick = () => {
             appendDots: postAppend,
           });
         }
->>>>>>> 0fe50595c657736e17efd32cf00ed6f617892bd1
       }
     });
   }
@@ -92,5 +42,44 @@ const customSlick = () => {
     destroySlider();
     initializeSlider();
   };
+
+
+  function benefitlistsSlider() {
+    const singleAppend = $(".benefit-lists-appends");
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      if (!$(".benefit-lists").hasClass("slick-initialized")) {
+        $(".benefit-lists").slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          prevArrow:
+            '<div class="slick-arrow slick-prev flex flex-center radius-50"><span class="slick-arrows slick-prev-arrow fa-solid fa-chevron-right"></span></div>',
+          nextArrow:
+            '<div class="slick-arrow slick-next flex flex-center radius-50"><span class="slick-arrows slick-next-arrow fa-solid fa-chevron-right"></span></div>',
+          dots: true,
+          speed: 1500,
+          infinite: true,
+          autoplay: false,
+          appendArrows: singleAppend,
+          appendDots: singleAppend,
+          responsive: [
+            {
+              breakpoint: 743,
+              settings: {
+                dots: true,
+              },
+            },
+          ],
+        });
+      } 
+  }else {
+      if ($(".benefit-lists").hasClass("slick-initialized")) {
+        $(".benefit-lists").slick("unslick");
+      }
+    }
+  }
+  benefitlistsSlider();
+
+  
 };
 export default customSlick;
