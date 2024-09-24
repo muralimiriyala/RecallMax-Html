7a15f7c8b6ca43d5dad07c004e44a823839cdba6
MuraliJbk <murali@jbkinfotech.com>
2024-09-24 20:11:42 +0530

    text1133889797

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
+import selectBox from './lib/jquery.selectBox';
 import Animations from './modules/animations';
 import Scroll from './modules/scroll';
 import stickyHeader from './modules/stickyHeader';
 import scrollModule from './modules/pageScroll';
 import customSlick from './modules/customSlick';
-import selectBox from './modules/selectbox';
 
 document.addEventListener('DOMContentLoaded', function () {
   Animations.init();
   stickyHeader.init();
   scrollModule.init();
   customSlick.init();
-  // selectBox.init();
+  selectBox.init();
 });
 
 let scrolled = window.scrollY;
