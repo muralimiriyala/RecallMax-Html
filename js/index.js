c6c7bd4d0abb9598f2fe3bae2e63b0bbb7f77689
MuraliJbk <murali@jbkinfotech.com>
2024-09-27 15:36:53 +0530

    272jsss

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ import selectBox from './lib/jquery.selectBox';
 import Animations from './modules/animations';
 import Scroll from './modules/scroll';
 import Menu from './modules/menu';
-import scrollModule from './modules/pageScroll';
+// import scrollModule from './modules/pageScroll';
 import customSlick from './modules/customSlick';
 import filterCategory from './modules/filterCategory';
 
 document.addEventListener('DOMContentLoaded', function () {
   Animations.init();
   Menu.init();
-  scrollModule.init();
+  // scrollModule.init();
   customSlick.init();
   selectBox.init();
   filterCategory.init();
--- js/modules/menu.js
+++ js/modules/menu.js
@@ -5,21 +5,25 @@ const Menu = {
   $links: document.querySelectorAll(
     'ul.main_menu > li.menu-item-has-children > a'
   ),
+  $overlay: document.querySelector('.header-overlay'),
   init() {
     const _ = this;
     // const $siteHeight = header.clientHeight;
     if (!_.$header) return false;
-    window.addEventListener('scroll', () => {
+    let userScroll = () => {
       const scroll = window.scrollY;
       scroll > 0
         ? _.$header.classList.add('sticky-header')
         : _.$header.classList.remove('sticky-header');
-    });
+    };
+    window.addEventListener('scroll', userScroll);
+    window.addEventListener('load', userScroll);
     let humburger = function (e) {
       e.preventDefault();
       const _$ = this;
       _$.classList.toggle('open');
       _.$nav.classList.toggle('open');
+      _.$overlay.classList.toggle('active');
     };
     if (!_.$btn) return false;
     _.$btn.addEventListener('click', humburger);
@@ -30,13 +34,30 @@ const Menu = {
       _$.classList.toggle('active');
       _.$links.forEach(($item) => {
         if ($item !== _$) {
+          $item.parentElement.querySelector('ul.sub-menu').style.maxHeight =
+            '0';
           $item.classList.remove('active');
         }
       });
+      let $submenu = _$.parentElement.querySelector('ul.sub-menu');
+      console.log(
+        $submenu.style.maxHeight && $submenu.style.maxHeight !== '0px'
+      );
+      $submenu.style.maxHeight && $submenu.style.maxHeight !== '0px'
+        ? ($submenu.style.maxHeight = '0')
+        : ($submenu.style.maxHeight = `${$submenu.scrollHeight}px`);
+    };
+    let onMouse = () => {
+      _.$overlay.classList.add('open');
+    };
+    let offMouse = () => {
+      _.$overlay.classList.remove('open');
     };
     if (!_.$links) return false;
     _.$links.forEach(function ($link) {
       $link.addEventListener('click', menu);
+      $link.addEventListener('mouseover', onMouse);
+      $link.addEventListener('mouseleave', offMouse);
     });
   },
 };
