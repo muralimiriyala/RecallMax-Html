acad52de3907a1f3dd0c7c5a5c5f0c70969b2ece
unknown <murali@jbkinfotech.com>
2024-10-05 21:38:05 +0530

    et

--- js/modules/testimonialSlider.js
+++ js/modules/testimonialSlider.js
@@ -2,9 +2,11 @@ const testimonialSlider = {
     $e: document.querySelectorAll('.testimonial-item'),
     init(){
         const $t = this;
+        const slides = Array.from($t.$e);
+        slides.at(0).classList.add('open')
         const slider = function(e){
            e.preventDefault();
-            // console.log($t.$e)
+            
         }
         $t.$e.forEach((ele)=>{
             ele.addEventListener('click', slider)
