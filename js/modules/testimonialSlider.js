af789159a3135695aa11a99030c124770691fe7c
unknown <murali@jbkinfotech.com>
2024-10-07 12:43:47 +0530

    et

--- js/modules/testimonialSlider.js
+++ js/modules/testimonialSlider.js
@@ -3,34 +3,41 @@ const testimonialSlider = {
     init(){
         const $t = this;
         const media = window.matchMedia('(min-width: 768px)');
-        console.log(media, "me")
+        const slides = Array.from($t.$e);
+        slides.at(0).classList.add('open');
+
+        const slider = function(e){
+            e.preventDefault();
+
+            const userWidth = 168;
+            const currentWidth = 556;
+            const target = e.currentTarget;
+            slides.forEach((slide)=>{
+                slide.classList.remove('open')
+                slide.style.flex = `0 0 ${userWidth}px`
+            })
+            target.classList.add('open');
+            target.style.flex =  `0 0 ${currentWidth}px`       
+        }
+        const mobileSlider = function(e){
+
+        };
+  
         if(media.matches){
-            const slider = function(e){
-                e.preventDefault();
-                const slides = Array.from($t.$e);
-                slides.at(0).classList.add('open');
-                const userWidth = 168;
-                const currentWidth = 556;
-                const target = e.currentTarget;
-                slides.forEach((slide)=>{
-                    slide.classList.remove('open')
-                    slide.style.flex = `0 0 ${userWidth}px`
-                })
-                target.classList.add('open');
-                target.style.flex =  `0 0 ${currentWidth}px`       
-            }
+            
             $t.$e.forEach((ele)=>{
                 ele.addEventListener('click', slider)
                 ele.addEventListener('change', slider)
+                ele.removeEventListener('click', mobileSlider)
+                ele.removeEventListener('change', mobileSlider)
             })
         }
         else{
-            const mobileSlider = function(e){
-
-            };
             $t.$e.forEach((ele)=>{
                 ele.addEventListener('click', mobileSlider)
                 ele.addEventListener('change', mobileSlider)
+                ele.addEventListener('click', slider)
+                ele.addEventListener('change', slider)
             })
         }
         
