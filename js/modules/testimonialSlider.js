44836c68593fa821f6653816f68401206a94522d
unknown <murali@jbkinfotech.com>
2024-10-07 11:56:04 +0530

    et

--- js/modules/testimonialSlider.js
+++ js/modules/testimonialSlider.js
@@ -2,15 +2,38 @@ const testimonialSlider = {
     $e: document.querySelectorAll('.testimonial-item'),
     init(){
         const $t = this;
-        const slides = Array.from($t.$e);
-        slides.at(0).classList.add('open')
-        const slider = function(e){
-           e.preventDefault();
-            
+        const media = window.matchMedia('(min-width: 768px)');
+        console.log(media, "me")
+        if(media.matches){
+            const slider = function(e){
+                e.preventDefault();
+                const slides = Array.from($t.$e);
+                slides.at(0).classList.add('open');
+                const userWidth = 168;
+                const currentWidth = 556;
+                const target = e.currentTarget;
+                slides.forEach((slide)=>{
+                    slide.classList.remove('open')
+                    slide.style.flex = `0 0 ${userWidth}px`
+                })
+                target.classList.add('open');
+                target.style.flex =  `0 0 ${currentWidth}px`       
+            }
+            $t.$e.forEach((ele)=>{
+                ele.addEventListener('click', slider)
+                ele.addEventListener('change', slider)
+            })
         }
-        $t.$e.forEach((ele)=>{
-            ele.addEventListener('click', slider)
-        })
+        else{
+            const mobileSlider = function(e){
+
+            };
+            $t.$e.forEach((ele)=>{
+                ele.addEventListener('click', mobileSlider)
+                ele.addEventListener('change', mobileSlider)
+            })
+        }
+        
     }
 }
 export default testimonialSlider;
