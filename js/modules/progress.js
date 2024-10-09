5ece9c1af437506598e21147fb9b0493b3aa8965
unknown <murali@jbkinfotech.com>
2024-10-09 11:57:42 +0530

    etyytytyty

--- js/modules/progress.js
+++ js/modules/progress.js
@@ -10,7 +10,7 @@ const progress = {
             $pathvalue = parseInt($pathvalue)
             const $progress = $dash - ($dash * $pathvalue / 100);
             $path.style.strokeDashoffset = $progress;
-            $path.style.transitionDuration = $duration;
+            $path.style.transitionDuration = `${$duration}s`;
         })
     },
     reset(){
@@ -18,6 +18,9 @@ const progress = {
         _.$circlepath.forEach($path=>{
             let $dash = $path.getAttribute('stroke-dasharray');
             $path.style.strokeDashoffset = Number($dash);
+            let $duration = Number($path.getAttribute('data-duration') / 1000);
+            $path.style.transitionDuration = `${$duration}s`;
+
         });
     }
 }
