32974c7c26ffdb24e91ba4a574c707b84fc403eb
unknown <murali@jbkinfotech.com>
2024-11-05 13:52:36 +0530

    testimonial

--- js/modules/numberInput.js
+++ js/modules/numberInput.js
@@ -1,23 +1,30 @@
-export const numberInput = function(){
-    (function quantityProducts() {
-        const $quantityArrowMinus = $(".quantity-arrow-minus");
-        const $quantityArrowPlus = $(".quantity-arrow-plus");
-        const $quantityNum = $(".quantity-num");
-    
-        let quantityMinus = (e) =>{
-          e.preventDefault();
-          if ($quantityNum.val() > 1) {
-            $quantityNum.val(+$quantityNum.val() - 1);
-          }
-        }
-        
-        let quantityPlus = (e) =>{
-          e.preventDefault();
-          $quantityNum.val(+$quantityNum.val() + 1);
-        }
+export const numberInput = function() {
+  (function quantityProducts() {
+      const quantityNum = document.querySelectorAll(".quantity-num");
 
-        $quantityArrowMinus.on("click", quantityMinus);
-        $quantityArrowPlus.on("click", quantityPlus);
-  
-    })();
-}
+      const quantityMinus = (e) => {
+          e.preventDefault();
+          const $input = e.target.parentElement.querySelector('input');
+          let currentValue = parseInt($input.value, 10) || 0;
+          if (currentValue > 1) {
+              $input.value = currentValue - 1;
+          }
+      };
+
+      const quantityPlus = (e) => {
+          e.preventDefault();
+          const $input = e.target.parentElement.querySelector('input');
+          let currentValue = parseInt($input.value, 10) || 0;
+          $input.value = currentValue + 1;
+      };
+
+      quantityNum.forEach(function($ele){
+        if($ele.closest('.quantity-block')){
+          const quantityArrowMinus = $ele.closest('.quantity-block').querySelector(".quantity-arrow-minus");
+          const quantityArrowPlus = $ele.closest('.quantity-block').querySelector(".quantity-arrow-plus");
+          quantityArrowMinus.addEventListener("click", quantityMinus.bind(this));
+          quantityArrowPlus.addEventListener("click", quantityPlus.bind(this));
+        }
+      })
+  })();
+};
