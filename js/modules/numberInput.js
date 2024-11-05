e0ed1e0ad7572666023902eca35bc03c5c17752b
unknown <murali@jbkinfotech.com>
2024-11-05 14:45:10 +0530

    testimonial

--- js/modules/numberInput.js
+++ js/modules/numberInput.js
@@ -2,28 +2,26 @@ export const numberInput = function() {
   (function quantityProducts() {
       const quantityNum = document.querySelectorAll(".quantity-num");
 
-      const quantityMinus = (e) => {
+      const quantityMinus = (e, $ele) => {
           e.preventDefault();
-          const $input = e.target.parentElement.querySelector('input');
-          let currentValue = parseInt($input.value, 10) || 0;
+          let currentValue = parseInt($ele.value, 10) || 0;
           if (currentValue > 1) {
-              $input.value = currentValue - 1;
+              $ele.value = currentValue - 1;
           }
       };
 
-      const quantityPlus = (e) => {
+      const quantityPlus = (e, $ele) => {
           e.preventDefault();
-          const $input = e.target.parentElement.querySelector('input');
-          let currentValue = parseInt($input.value, 10) || 0;
-          $input.value = currentValue + 1;
+          let currentValue = parseInt($ele.value, 10) || 0;
+          $ele.value = currentValue + 1;
       };
 
       quantityNum.forEach(function($ele){
         if($ele.closest('.quantity-block')){
           const quantityArrowMinus = $ele.closest('.quantity-block').querySelector(".quantity-arrow-minus");
           const quantityArrowPlus = $ele.closest('.quantity-block').querySelector(".quantity-arrow-plus");
-          quantityArrowMinus.addEventListener("click", quantityMinus.bind(this));
-          quantityArrowPlus.addEventListener("click", quantityPlus.bind(this));
+          quantityArrowMinus.addEventListener("click", (e) => quantityMinus(e, $ele));
+          quantityArrowPlus.addEventListener("click", (e) => quantityPlus(e, $ele));
         }
       })
   })();
