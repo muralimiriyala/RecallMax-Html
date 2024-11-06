9fea62fc50b644c24ef7cdbe2e0a1ebefcf4b9c2
unknown <murali@jbkinfotech.com>
2024-11-06 15:58:37 +0530

    testimonial

--- js/modules/progress.js
+++ js/modules/progress.js
@@ -1,8 +1,25 @@
 const svgprogress = {
     $max: document.querySelector('.max-total'),
+    $circlesvg: document.querySelectorAll('.round-default-svg'),
     $circlepath: document.querySelectorAll('[data-count-path]'),
+    $circleto: document.querySelectorAll('[data-count-to]'),
     init(){
         const _=this;
+
+        let $svgvalues = [];
+        _.$circleto.forEach(($item, $i) => {
+            $svgvalues[$i] = $item.dataset.countTo;
+        });
+        if (!_.$circleto.length) return;
+        _.$circlesvg.forEach(($ele, $index) => {
+            let pathsInSvg = $ele.querySelectorAll('circle');
+            if (!pathsInSvg.length) return;
+            pathsInSvg.forEach(($path, pathIndex) => {
+                $path.setAttribute('data-count-path', $svgvalues[$index]);
+            });
+        });
+
+
         if(!_.$max) return;
         let start = parseInt(_.$max.innerHTML.replace(/,/g, ''), 10) || 0;
         if(start<0) return;
