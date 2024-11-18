65eb4354d35d1cd0a35459ef4e98357bb32439e4
MuraliJbk <murali@jbkinfotech.com>
2024-11-18 18:13:10 +0530

    slider

--- js/modules/tabs-filter.js
+++ js/modules/tabs-filter.js
@@ -1,5 +1,3 @@
-import { cssNumber } from "jquery";
-
 const tabFilter = {
   $ele: document.querySelectorAll('.tab-links-d'),
   $tabs: document.querySelectorAll(`.tab-links-main`),
@@ -8,24 +6,27 @@ const tabFilter = {
     let tabFun = function (e) {
       e.preventDefault();
       let $target = e.target;
-      _.$ele.forEach((ele)=>{
-        if(ele !== $target){
-          ele.classList.remove('open')
+      _.$ele.forEach((ele) => {
+        if (ele !== $target) {
+          ele.classList.remove('open');
         }
-      })
-      if($target.tagName==="SPAN") $target = e.target.parentElement;
-      if($target.tagName==="svg") $target = e.target.parentElement.parentElement;
-    
-      const tabName = $target.getAttribute("data-name");
-      const $tabele = document.querySelector(`.tab-links-main[data-value=${tabName}]`);
+      });
+      if ($target.tagName === 'SPAN') $target = e.target.parentElement;
+      if ($target.tagName === 'svg')
+        $target = e.target.parentElement.parentElement;
+
+      const tabName = $target.getAttribute('data-name');
+      const $tabele = document.querySelector(
+        `.tab-links-main[data-value=${tabName}]`
+      );
       if ($tabele.length < 0) return;
-      _.$tabs.forEach((tab)=>{
-        if(tab !== $tabele){
+      _.$tabs.forEach((tab) => {
+        if (tab !== $tabele) {
           $target.classList.remove('open');
           tab.style.maxHeight = '';
           tab.dataset.id = 'false';
         }
-      })
+      });
       if ($tabele && $tabele.dataset.id !== 'true') {
         $target.classList.toggle('open');
         $tabele.style.maxHeight = `${$tabele.scrollHeight}px`;
@@ -37,9 +38,9 @@ const tabFilter = {
       }
     };
     if (!_.$ele.length < 0) return;
-    _.$ele.forEach(($ele)=>{
+    _.$ele.forEach(($ele) => {
       $ele.addEventListener('click', tabFun);
-    })
+    });
   },
 };
 export default tabFilter;
