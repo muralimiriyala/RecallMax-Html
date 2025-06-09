a7442aeceea02ad121ef77e9dd0dc49a5ee98f92
MuraliJbk <murali@jbkinfotech.com>
2025-06-09 18:58:17 +0530

    slider

---
+++ dist/custom-iframe.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function () {
+  jQuery('[data-vbg]').youtube_background({});
+});
+
+// jQuery(document).ready(function () {
+//   jQuery('[data-vbg]').youtube_background({
+//     'play-button': true,
+//     volume: 1,
+//     mute: false,
+//   });
+// });
+// jQuery(document).one('click', function () {
+//   jQuery('[data-vbg]').each(function () {
+//     if (this.player && typeof this.player.unMute === 'function') {
+//       this.player.unMute();
+//       this.player.setVolume(100);
+//     }
+//   });
+// });
